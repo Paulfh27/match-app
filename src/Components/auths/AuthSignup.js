2eca3128eb1e0a17dbc30afb38f91b3536ee4b64
Paulfh27 <paulfhebertjr@gmail.com>
2024-06-13 15:53:50 -0400

    small cleanup unused code

--- src/Components/auths/AuthSignup.js
+++ src/Components/auths/AuthSignup.js
@@ -4,7 +4,6 @@ import { useState } from "react";
 import { useAuth } from '../../contexts/AuthContext'
 
 export const AuthSignup = (props) => {
-  const { user } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { login } = useAuth();
