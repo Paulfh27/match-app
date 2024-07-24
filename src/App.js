d2c1a7acec2ccb92774cd89701b24bad6707d907
Paulfh27 <paulfhebertjr@gmail.com>
2024-07-24 14:26:24 -0400

    made the matches page only appear when user is logged in using useAuth context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+import { useAuth } from "./contexts/AuthContext";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Home from "./pages/Home";
 import Matches from "./pages/Matches";
 
 function App() {
+  const { user } = useAuth();
+  // QUESTION: should I even make the route unavailable if there is no option to navigate to it on the tool bar
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Main" element={<Main />} />
+        { user != null ? (
         <Route path="/Matches" element={<Matches />} />
+        ) : null}
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -1,10 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
 import "./toolbar.css";
+import { useAuth } from "../contexts/AuthContext";
 
 // Toolbar Class
 //      Sits at the bottom of the screen in most pages
 //      holds all navigation options
 function Toolbar(props) {
+  const { user } = useAuth();
+
   return (
     <div className="toolbar">
       <ul className="nav-links">
@@ -14,9 +17,11 @@ function Toolbar(props) {
         <li>
           <Link to="/Main">Main</Link>
         </li>
+        { user != null ? (
         <li>
           <Link to="/Matches">Matches</Link>
         </li>
+        ) : null }
       </ul>
     </div>
   );
